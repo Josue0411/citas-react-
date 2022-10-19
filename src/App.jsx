fa4844e792c207522b059534b73ee96dea84779f
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-19 17:48:27 -0400

    maquetando el header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Formulario from './components/Formulario';
-import Header from './components/Header'
-import ListadoPaciente from './components/ListadoPaciente';
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
+import ListadoPaciente from "./components/ListadoPaciente";
 function App() {
   return (
-    <>
-    <Header/>
-    <Formulario/>
-    <ListadoPaciente/>
-    </>
+    <div className="container mx-auto mt-20">
+      <Header />
+      <Formulario />
+      <ListadoPaciente />
+    </div>
   );
 }
 
