7475b3667a79ca2616b8a1ee53a4617466ecffdf
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-20 15:19:46 -0400

    mensajes de rror con children finalizados y todo funcional

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-const Formulario = () => {
+import Error from './Error'
+const Formulario = ({ pacientes,setPacientes }) => {
   const [nombre, setNombre] = useState("");
   const [propietario, setPropietario] = useState("");
   const [email, setEmail] = useState("");
@@ -14,6 +15,21 @@ const Formulario = () => {
       return;
     }
     setError(false);
+    //objeto de pacientes
+    const objetoPaciente = {
+      nombre,
+      propietario,
+      email,
+      fecha,
+      sintomas,
+    };
+    setPacientes([...pacientes,objetoPaciente]);
+    //Reiniciar el formulario
+    setNombre('')
+    setPropietario('')
+    setEmail('')
+    setFecha('')
+    setSintomas('')
   };
   return (
     <div className="md:w-1/2 lg:w-2/5">
@@ -26,11 +42,7 @@ const Formulario = () => {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mx-5"
       >
-        {error && (
-          <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
-            <p>Todos los campos son obligatorios</p>
-          </div>
-        )}
+        {error && <Error> <p>Todos los campos son obligatorios</p></Error> }
         <div className="mb-5">
           <label
             htmlFor="mascota"
--- src/components/ListadoPaciente.jsx
+++ src/components/ListadoPaciente.jsx
@@ -1,6 +1,7 @@
 import Paciente from "./Paciente";
 
-const ListadoPaciente = () => {
+const ListadoPaciente = ({ pacientes }) => {
+  console.log(pacientes);
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       <h2 className="font-black text-3xl text-center">Listado pacientes</h2>
@@ -8,13 +9,8 @@ const ListadoPaciente = () => {
         Administra tus {""}
         <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
       </p>
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>
-        <Paciente/>
+
+      <Paciente />
     </div>
   );
 };
