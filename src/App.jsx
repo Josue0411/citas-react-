500c04e322d9142f30da3c86c70aa246a6ccc73c
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-21 10:59:07 -0400

    detectar un registro nuevo o una edicion

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="container mx-auto mt-20">
       <Header />
       <div className="mt-12 md:flex">
-        <Formulario pacientes={pacientes} setPacientes={setPacientes} paciente={paciente}  />
+        <Formulario pacientes={pacientes} setPacientes={setPacientes} paciente={paciente} setPaciente={setPaciente}  />
         <ListadoPaciente pacientes={pacientes} setPaciente={setPaciente} />
       </div>
     </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Error from "./Error";
-const Formulario = ({ pacientes, setPacientes, paciente }) => {
+const Formulario = ({ pacientes, setPacientes, paciente,setPaciente }) => {
   const [nombre, setNombre] = useState("");
   const [propietario, setPropietario] = useState("");
   const [email, setEmail] = useState("");
@@ -9,12 +9,12 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    if(Object.keys(paciente).length > 0){
-      setNombre(paciente.nombre)
-      setPropietario(paciente.propietario)
-      setEmail(paciente.email)
-      setFecha(paciente.fecha)
-      setSintomas(paciente.sintomas)
+    if (Object.keys(paciente).length > 0) {
+      setNombre(paciente.nombre);
+      setPropietario(paciente.propietario);
+      setEmail(paciente.email);
+      setFecha(paciente.fecha);
+      setSintomas(paciente.sintomas);
     }
   }, [paciente]);
 
@@ -38,9 +38,19 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
       email,
       fecha,
       sintomas,
-      id: generarId(),
     };
-    setPacientes([...pacientes, objetoPaciente]);
+    if (paciente.id) {
+      //Editando registro
+      objetoPaciente.id = paciente.id;
+
+      const pacientesActualizados=pacientes.map(pacienteState=>pacienteState.id===paciente.id ? objetoPaciente : pacienteState)
+      setPacientes(pacientesActualizados)
+      setPaciente({})
+    } else {
+      //Nuevo registro
+      objetoPaciente.id = generarId();
+      setPacientes([...pacientes, objetoPaciente]);
+    }
     //Reiniciar el formulario
     setNombre("");
     setPropietario("");
@@ -149,7 +159,7 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
         <input
           type="submit"
           className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-          value={paciente.id ? 'Editar paciente' : 'Agregar paciente'}
+          value={paciente.id ? "Editar paciente" : "Agregar paciente"}
         />
       </form>
     </div>
